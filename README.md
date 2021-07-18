16ab676909c3ae1521d4d02be89d70cf90bde99d
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2021-07-18 16:15:43 -0300

    Adcionando arquivos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ShellScripts
+Antes de executar estes arquivos lembre de dar a permissão de execução (chmod +x) e de executar todos com permissão de root (sudo arquivo.sh)
---
+++ installflatpak.sh
@@ -0,0 +1,3 @@
+apt -y install flatpak
+apt -y install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo

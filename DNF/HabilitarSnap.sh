8b466ef8a19dd661c44a745c1af8be335c09ce1d
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2021-08-24 21:56:23 -0300

    Adicionando arquivos de shell script para dnf

---
+++ DNF/HabilitarSnap.sh
@@ -0,0 +1,2 @@
+dnf -y install snapd
+ln -s /var/lib/snapd/snap /snap
---
+++ DNF/vagrantinst.sh
@@ -0,0 +1,3 @@
+dnf install -y dnf-plugins-core
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+dnf -y install vagrant
